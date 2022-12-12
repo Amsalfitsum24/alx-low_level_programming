50ce10ff571e361aa354dfeb72cf2ca9413494f4
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-12 13:18:21 -0800

    git add 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+	return (0);
 }
