786475139b5ec98365f84f68f2eeba1864c231b8
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-12 12:31:34 -0800

    progrrame

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main prints phrase
+ * outcome very good 
+ * Return: 0 exited properly
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
