d84b33693da09ce67d2b5ea5098e02aa811e7cd7
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-12 13:10:14 -0800

    gitadd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints size,
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
