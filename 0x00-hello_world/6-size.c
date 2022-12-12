f33309c126ed363bd72c79d9df039c14a486ae43
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-12 13:14:09 -0800

    git add 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
