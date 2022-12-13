ee462fc83a158050c1e94cbb2e546119f443aca3
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-12 18:57:14 -0800

    102-print_comb5.c 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,10 @@ int main(void)
 	int bas;
 
 for ( tho = 0 ; tho < 10 ; tho++)
-{	
+{
 
 	for (hun = 0 ; hun <= 10 ; hun++)
 	{
-											
 		for (ten = 0 ; ten < 10 ; ten++)
 		{
 			for ( bas = 0 ; bas < 10 ; bas++)
