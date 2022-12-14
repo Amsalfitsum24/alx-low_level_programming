90c22ea052f23708155750263a42faab81700ab2
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-14 15:02:05 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the english alphabet from a-z.
+ * print_alphabet_x10 - repeats the print_alphabet 10 times.
  * Return: Nothing.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 
 {
+	int i;
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i < 10; ++i)
 	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
 		_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
