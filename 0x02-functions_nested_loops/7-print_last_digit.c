deae11afc55f978e9d6a289e81bf81ade8a2df36
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-14 15:28:56 -0800

    7-print_last_digit.c 2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,13 @@
 int print_last_digit(int n)
 
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int end;
+	
+	end = n % 10;
+	if (end < 0)
+	{
+		end = end * -1;
+	}
+	_putchar(end + '0');
+	return (end`);
 }
