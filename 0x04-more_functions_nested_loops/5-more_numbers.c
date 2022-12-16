cf8b6aaf72694ced358ed0c3e19f220e59d90271
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-16 12:36:35 -0800

    4 5 tasks again

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *   * more_numbers - Print 10 times the numbers since 0 up to 14
- *     *
- *       * Return: 10 times of the numbers since 0 up to 14
- *         */
+ * more_numbers - Print 10 times the numbers since 0 up to 14
+ *
+ * Return: 10 times of the numbers since 0 up to 14
+ */
 
 void more_numbers(void)
 
