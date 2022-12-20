fdf54b36a949c9270610742620a393efe7c30961
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-20 14:05:27 -0800

    0x05-pointers_arrays_stringsdfg num 8 ag

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,6 +5,7 @@
  * print_array - a function that prints n elements of an array
  * @a: array name
  * @n: is the number of elements OF the array to be printed
+ *
  * Return: a and n inputs
  */
 
@@ -15,8 +16,7 @@ void print_array(int *a, int n)
 	for (j = 0; j < n; j++)
 	{
 		printf("%d", a[j]);
-
-		if (j != (n - 1))
+		if (j != n - 1)
 		{
 			printf(",");
 			printf(" ");
