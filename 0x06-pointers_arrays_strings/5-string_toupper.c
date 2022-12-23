0bc5e8c24c01c783bc941346bdf9c6feed1340ee
Amsal fitsum <amsalfessha21@gmail.com>
2022-12-23 14:47:36 -0800

    insta -----5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *    string_toupper - Changes all lowercase letters to uppercase
- *      @p: The string will be modified
- *       
- *          Return: char var
+ * string_toupper - Changes all lowercase letters to uppercase
+ * @p: The string will be modified
+ *
+ * Return: char var
  */
 
 char *string_toupper(char *p)
