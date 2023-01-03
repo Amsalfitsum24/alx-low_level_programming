9c301736326e73156f2a6214cdae1594c14d54db
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-03 13:38:46 -0800

    kilod

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,22 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, n, total1 = 0, total2 = 0;
+	int i, size1; 
+	unsigned int total1, total2;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
+	size1 = 0;
+	total1 = 0;
+	total2 = 0;
+
+	size1 = (size * size) - 1;
+
+	for (i = 0; i <= size1; i = i + (size + 1))
+	{
 		total1 = total1 + a[i];
-
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	}
+	for (i = (size - 1); i < size1; i = i + (size - 1))
+	{
 		total2 = total2 + a[n];
+	}
 	printf("%d, %d\n", total1, total2);
 }
