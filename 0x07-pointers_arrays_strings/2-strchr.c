d0ab39bd1d748296092f8cd018a9d208c81fbf50
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-03 10:58:14 -0800

    brtu

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _strchr -Entry point
+ * _strchr - Entry point
  * @s: input
  * @c: input
  * Return: Always 0 (success)
@@ -11,9 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+
 	for (i = 0; s[i] >= '\0'; i++)
+
 	{
-		if (s[i] == c)
+		if (s[i] == c
+				)
 			return (s + i)
 	}
 
