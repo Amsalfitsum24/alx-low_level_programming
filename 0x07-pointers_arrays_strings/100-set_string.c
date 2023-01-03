06241145f391ffa8241b4311c32b566bb6bd1fa9
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-03 13:20:12 -0800

    hahaha

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointerto a char.
+ * @s: pointer to pointer.
+ * @to: pointer to char.
+ *
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
