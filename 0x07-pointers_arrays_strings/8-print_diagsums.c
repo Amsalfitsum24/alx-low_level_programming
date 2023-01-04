ea2d7348783a6ed9ec8a42d11bfdc80d9916962f
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-03 18:27:24 -0800

    mkis

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,29 +2,19 @@
 #include "main.h"
 
 /**
- * print_diagsum -Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
- */
+ *  * print_diagsums - Entry point
+ *   * @a: input
+ *    * @size: input
+ *     * Return: Always 0 (Success)
+ *      */
 void print_diagsums(int *a, int size)
 {
-	int i, size1; 
-	unsigned int total1, total2;
+	int i, n, total1 = 0, total2 = 0;
 
-	size1 = 0;
-	total1 = 0;
-	total2 = 0;
-
-	size1 = (size * size) - 1;
-
-	for (i = 0; i <= size1; i = i + (size + 1))
-	{
+	for (i = 0; i <= (size * size); i = i + size + 1)
 		total1 = total1 + a[i];
-	}
-	for (i = (size - 1); i < size1; i = i + (size - 1))
-	{
-		total2 = total2 + a[i];
-	}
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		total2 = total2 + a[n];
 	printf("%d, %d\n", total1, total2);
 }
