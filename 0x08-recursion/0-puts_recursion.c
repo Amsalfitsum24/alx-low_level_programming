2b38598e1bdade900cf184ee197587d68084dd04
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-05 10:23:55 -0800

    amsi is ine

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line 
+ * _puts_recursion - Prints a string followed by a new line
  * @s: string
  *
  * Return: On success 1.
