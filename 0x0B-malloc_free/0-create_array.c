515290f07cd3f8b1665c2755fcb12c7fb6dd0f61
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-11 11:53:30 -0800

    kiloskkj

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,56 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
+
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * create_array - create an arry of size with char c
+ * @size: unsigned int type
+ * @c: char type
+ * Return: Return pointer to array created
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
+	char *s;
 	unsigned int i;
 
 	if (size == 0)
+	{
 		return (NULL);
-	
-	arr = malloc(size * sizeof(char));
-	if (arr == NULL)
+	}
+
+	s = malloc((size) * sizeof(char));
+	if (s == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; i < size; i++)
-		arr[i] = c;
-
-	return (arr);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
