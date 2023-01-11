6a59074e8f93d78a1f65e6fa10f863bc2fa621eb
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-11 07:53:15 -0800

    missi is the best

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if the given character is lower case.
+ * @c: character to test.
+ *
+ * Return: 1 (True) else 0 (False)
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
+
