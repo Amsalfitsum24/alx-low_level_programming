15c0f0aea46e4fac97961272d34821c99fc2f4ba
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-13 13:40:10 -0800

    taskme

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - ...
+ * @s1: ...
+ * @s2: ...
+ * @n: ...
+ *
+ * Return: ...
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		1 = i + k;
+	else
+		1 = i + n;
+
+	str = malloc(sizeof(char) *1 + 1);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	while (j < 1)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '/0';
+	return (str);
+}
