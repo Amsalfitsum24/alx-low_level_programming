e17051651ee0c9beccd185b104d4451fe5de02cb
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-13 12:24:53 -0800

    task1

--- 0x0c-more_malloc_free/0-malloc_checked.c
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *malloc_checked - array for prints a string
- *@b: number of memory
- *Return: void
+ * malloc_checked - Allocates memory
+ * @b: the size to allocate
+ *
+ * Return: Nothing.
  */
-
 void *malloc_checked(unsigned int b)
 {
-void *p;
+	void *p;
 
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
+	p = malloc(b);
 
-return (p);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
 }
