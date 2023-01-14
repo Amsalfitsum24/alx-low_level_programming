6d51a752958c6613078e06a782d806fc1ddc0e08
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-13 17:16:12 -0800

    amsiye

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - reallocates memory for a pointer
+ *
+ * @ptr: pointer to old memory allocated
+ * @old_size: size of memory before
+ * @new_size: size of memory after
+ * Return: pointer to new memory allocated
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new;
+	unsigned int i, min;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		new = malloc(new_size);
+		return (new);
+	}
+	else if (new_size == 0)
+	{
+		ree(ptr);
+		return (NULL);
+	}
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		min = old_size;
+	else
+		min = new_size;
+	for (i = 0; i < min; i++)
+		new[i] = *((char *)ptr + i);
+	free(ptr);
+	return ((void *)new);
+}
+
