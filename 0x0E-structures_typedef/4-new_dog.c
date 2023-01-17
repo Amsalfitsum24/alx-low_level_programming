97a8131b6a0f43ab86629d2040a8cf3a1112003e
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-17 15:36:19 -0800

    this is my last 1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,7 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		if (cutie_dog->name == NULL)
 		{
 			free(cutie_dog);
-			return (NULL); 
+			return (NULL);
 		}
 
 		cutie_dog->owner = malloc(sizeof(char) * own_l);
