521ed67c95a2e970214590108911ef85c55b45c0
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-19 13:13:20 -0800

    taskmee

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,14 +3,10 @@
 #include <stdio.h>
 
 /**
- *
- *  * sum_them_all - Entry Point
- *
- *   * @n: variadic inputs
- *
- *    * Return: sum
- *
- *     */
+ *  sum_them_all - Entry Point
+ *  @n: variadic inputs
+ *  Return: sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,1 @@
-task
+bhakii
