bdfd59a942393b5018c1be53360b8521ab45068d
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-19 14:05:27 -0800

    tired

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,81 +1,41 @@
 #include <stdio.h>
-
 #include "variadic_functions.h"
-
 #include <stdarg.h>
 
-
-
 /**
- *
- *  * print_strings - prints strings.
- *
- *   * @n: number of arguments
- *
- *    * @separator: character separator of numbers
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * print_strings - Prints strings.
+ * @n: number of arguments
+ * @separator: character separator of numbers
+ * Return: Always 0.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
-
 {
+	va_list string;
+	unsigned int i;
+	char *str;
 
-		va_list string;
+	if (separator == NULL)
+	{
+		separator = "";
+	}
 
-			unsigned int i;
+	va_start(string, n);
 
-				char *str;
-
-
-
-					if (separator == NULL)
-
-							{
-
-										separator = "";
-
-											}
-
-
-
-						va_start(string, n);
-
-
-
-							for (i = 0; i < n; i++)
-
-									{
-
-												str = va_arg(string, char *);
-
-														if (str == NULL)
-
-																	{
-
-																					printf("(nil)");
-
-																								break;
-
-																										}
-
-																printf("%s", str);
-
-																		if (n == i + 1)
-
-																					{
-
-																									break;
-
-																											}
-
-																				printf("%s", separator);
-
-																					}
-
-								printf("\n");
-
-									va_end(string);
-
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(string, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+			break;
+		}
+		printf("%S", str);
+		if (n == i + 1)
+		{
+			break;
+		}
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(string);
 }
