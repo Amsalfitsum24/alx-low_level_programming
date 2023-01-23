96b927856b314176551dd08119e405216a218236
Amsal fitsum <amsalfessha21@gmail.com>
2023-01-23 08:40:47 -0800

    miko

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,55 +1,44 @@
 #include "variadic_functions.h"
-
 #include <stdarg.h>
-
 #include <stdio.h>
 
 /**
  *
- *  * print_numbers - Entry Point
+ * print_numbers - Entry Point
  *
- *   * @separator: comma space
+ * @separator: comma space
  *
- *    * @n: elements to be printed
+ * @n: elements to be printed
  *
- *     * Return: void
+ * Return: void
  *
- *      */
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 
 {
 
-		char *sep;
+	char *sep;
 
-			unsigned int i;
+	unsigned int i;
 
-				va_list list;
+	va_list list;
 
+	if (separator == NULL || *separator == 0)
+		sep = "";
+	else
+		sep = (char *) separator;
 
+	va_start(list, n);
 
-					if (separator == NULL || *separator == 0)
+	if (n > 0)
+		printf("%d", va_arg(list, int));
 
-								sep = "";
+	for (i = 1; i < n; i++)
+		printf("%s%d", sep, va_arg(list, int));
 
-						else
+	printf("\n");
 
-									sep = (char *) separator;
-
-							va_start(list, n);
-
-
-
-								if (n > 0)
-
-											printf("%d", va_arg(list, int));
-
-									for (i = 1; i < n; i++)
-
-												printf("%s%d", sep, va_arg(list, int));
-
-										printf("\n");
-
-											va_end(list);
+	va_end(list);
 
 }
