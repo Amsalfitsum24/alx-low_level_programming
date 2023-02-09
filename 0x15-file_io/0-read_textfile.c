40380da3fe0413f76cef5fb7e16ef37f83651205
Amsal fitsum <amsalfessha21@gmail.com>
2023-02-09 07:02:59 -0800

    hostile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - read a text file and print it to stdout
- * @filename: the name of the file to read
- * @letters: the number of letters to be read and printed
- *
- * Return: If filename is NULL, the file cannot be opened or read, or
- * write fails or returns an unexpected number of bytes, return 0.
- * Otherwise, return the actual number of letters read and printed.
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = NULL;
-	ssize_t b_read;
-	ssize_t b_written;
-	int fd;
+	ssize_t file, let, w;
+	char *text;
 
-	if (!(filename && letters))
+	text = malloc(letters);
+	if (text == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
+	file = open(filename, O_RDONLY);
 
-	b_read = read(fd, buffer, letters);
-	close(fd);
-
-	if (b_read < 0)
+	if (file == -1)
 	{
-		free(buffer);
+		free(text);
 		return (0);
 	}
-	if (!b_read)
-		b_read = letters;
 
-	b_written = write(STDOUT_FILENO, buffer, b_read);
-	free(buffer);
+	let = read(file, text, letters);
 
-	if (b_written < 0)
-		return (0);
+	w = write(STDOUT_FILENO, text, let);
 
-	return (b_written);
+	close(file);
+
+	return (w);
 }
