eb10007da36dab95d46034e69ea5b827bf69eed8
amsal fitsum <amsalfessha21@gmail.com>
2023-03-24 13:14:08 -0700

    tasklibrary3

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -P /tmp/ https://raw.github.com/amsalfitsum24/alx-low_level_programming/master/0x18-dynamic_libraries/100-operations.so
+export LD_PRELOAD=/path/to/100-operations.so
+
